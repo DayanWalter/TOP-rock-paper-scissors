d8e97eb57e9328405efd54e1953f03f87de62e15
SynthCyraX <SynthCyraX@gmail.com>
2023-02-03 07:04:30 +0100

    Added rules in pseudocode

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -9,8 +9,22 @@ function getComputerChoice(){
 
 // present the choice
 const computerSelection = getComputerChoice();
+console.log(computerSelection);
 //the player chooses 1 of 3 options
 const playerSelection = "rock";
 
-console.log(computerSelection);
-console.log(playerSelection);
+//computer choose and player choose
+//compare booth choices
+//if computer choose rock and player choose rock - again
+//if compoter choose rock and player choose paper - player won
+//if computer choose rock and player choose scissors - computer won
+//if computer choose paper and player choose rock - computer won
+//if computer choose paper and player choose paper - again
+//if computer choose paper and player choose scissors - player won
+//if computer choose scissors and player choose rock - player won
+//if computer choose scissors and player choose paper - computer won
+//if computer choose scissors and player choose scissors - again 
+function playRound(playerSelection, computerSelection){
+    if (computerSelection = "Rock"){}
+}
+console.log(playRound(playerSelection, computerSelection));
