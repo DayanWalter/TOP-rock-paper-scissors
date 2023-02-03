5f735ea613f102b8870746f77713cedc6e158762
SynthCyraX <SynthCyraX@gmail.com>
2023-02-03 10:57:11 +0100

    Added 3 constants for every situation possible

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -11,20 +11,58 @@ function getComputerChoice(){
 const computerSelection = getComputerChoice();
 console.log(computerSelection);
 //the player chooses 1 of 3 options
-const playerSelection = "rock";
+const playerSelection = "paper";
+
 
 //computer choose and player choose
 //compare booth choices
-//if computer choose rock and player choose rock - again
-//if compoter choose rock and player choose paper - player won
-//if computer choose rock and player choose scissors - computer won
-//if computer choose paper and player choose rock - computer won
-//if computer choose paper and player choose paper - again
-//if computer choose paper and player choose scissors - player won
-//if computer choose scissors and player choose rock - player won
-//if computer choose scissors and player choose paper - computer won
-//if computer choose scissors and player choose scissors - again 
 function playRound(playerSelection, computerSelection){
-    if (computerSelection = "Rock"){}
+
+// if 
+// computer choose rock & player choose rock
+// or
+// computer choose paper & player choose paper
+// or
+// computer choose scissors & player choose scissors
+// than
+// stale
+const stale = ((computerSelection == "Rock") & (playerSelection == "rock") || 
+(computerSelection == "Paper") & (playerSelection == "paper") ||
+(computerSelection == "Scissors") & (playerSelection == "scissors"))
+
+// if
+// computer choose rock & player choose paper
+// or
+// computer choose paper & player choose scissors
+// or
+// computer choose scissors & player choose rock
+// than
+// player won
+const playerWon = (computerSelection == "Rock") & (playerSelection == "paper") || 
+(computerSelection == "Paper") & (playerSelection == "scissors") ||
+(computerSelection == "Scissors") & (playerSelection == "rock")
+
+// if
+// computer choose rock & player choose scissors
+// or
+// computer choose paper & player choose rock
+// or
+// computer choose scissors & player choose paper
+// than
+// computer won
+const computerWon = (computerSelection == "Rock") & (playerSelection == "scissors") || 
+(computerSelection == "Paper") & (playerSelection == "rock") ||
+(computerSelection == "Scissors") & (playerSelection == "paper")
+
+// If statement with all constants
+if(stale){
+    console.log("Stale!")
+}else if(playerWon){
+    console.log("Player won!")
+}else if(computerWon){
+    console.log("Computer won!")
+}else{
+    console.log("What happened?!")
 }
-console.log(playRound(playerSelection, computerSelection));
+}
+console.log(playRound());
